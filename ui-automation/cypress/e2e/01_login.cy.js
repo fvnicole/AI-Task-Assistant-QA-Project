56de9e7f4bda21099901b19a0f90e2b9c1ba4cf0
Cole Fulcar <fulcarnicole@gmail.com>
2026-09-24 14:35:10 -0400

    Update 01_login.cy.js

--- ui-automation/cypress/e2e/01_login.cy.js
+++ ui-automation/cypress/e2e/01_login.cy.js
@@ -21,7 +21,11 @@ describe('Login Suite', () => {
   });
 
   it('TC03 — Wrong email format', () => {
-    // ...
+    cy.get('#auth-email').type('Test01mail.com')
+      .then(($input) => {
+    expect($input[0].validationMessage).to.contain("@");
+
+  });
   });
 
   it('TC04 — Registered email + wrong password', () => {
@@ -46,19 +50,42 @@ describe('Login Suite', () => {
   });
 
   it('TC06 — Empty email field', () => {
-    // ...
+       cy.get('#auth-email').clear();
+   cy.get('#auth-password').type('12345678')
+   cy.get('[data-cy="auth-submit"]').click()
+   cy.get('[data-cy="auth-error"]')
+  .should('be.visible')
+  .and('contain', "Email can't be empty.");   
+
   });
 
   it('TC07 — Empty password field', () => {
-    // ...
+    
+       cy.get('#auth-email').type('Test01@mail.com')
+   cy.get('#auth-password').clear();
+   cy.get('[data-cy="auth-submit"]').click()
+   cy.get('[data-cy="auth-error"]')
+  .should('be.visible')
+  .and('contain', "Password must be at least 8 characters.");   
   });
 
   it('TC08 — Password < 8 characters', () => {
-    // ...
+     cy.get('#auth-email').type('Test01@mail.com')
+   cy.get('#auth-password').type('123')
+   cy.get('[data-cy="auth-submit"]').click()
+   cy.get('[data-cy="auth-error"]')
+  .should('be.visible')
+  .and('contain', "Password must be at least 8 characters."); 
   });
 
   it('TC09 — Both fields empty', () => {
-    // ...
+   cy.get('#auth-email').clear();
+   cy.get('#auth-password').clear();
+   cy.get('[data-cy="auth-submit"]').click()
+   cy.get('[data-cy="auth-error"]')
+  .should('be.visible')
+  .and('contain', "Email can't be empty.");   
+
   });
 
 });
